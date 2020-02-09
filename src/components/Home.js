5863db67d7fa0adf827ec5c701b9fc64b05a7efb
appdevdude7 <appdeveloper.akshay@gmail.com>
2020-02-09 10:26:07 -0600

    added home page

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <HeroImage />
+            <SearchBar />
+            <Grid />
+        </div>
+    )
+}
+
+export default Home;
