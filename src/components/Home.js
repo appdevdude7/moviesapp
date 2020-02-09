768d42437b7ee0a6bc94721c8b5dd9e2e84a2826
appdevdude7 <appdeveloper.akshay@gmail.com>
2020-02-09 10:29:15 -0600

    added components onto home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
+import HeroImage from './elements/HeroImage'
+import SearchBar from './elements/SearchBar'
+import Grid from './elements/Grid'
+import MovieThumb from './elements/MovieThumb'
+import Spinner from './elements/Spinner'
+import LoadMoreBtn from './elements/LoadMoreBtn'
+
 const Home = () => {
     return (
-        <div>
+        <>
             <HeroImage />
             <SearchBar />
             <Grid />
-        </div>
+            <MovieThumb />
+            <Spinner />
+            <LoadMoreBtn />
+        </>
     )
 }
 
