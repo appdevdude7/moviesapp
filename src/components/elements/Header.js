849c385d01ed0c58e5be07135c64aa49732f9e67
appdevdude7 <appdeveloper.akshay@gmail.com>
2020-02-09 14:11:58 -0600

    added styled component files

--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
+import RMDBLogo from '../images/reactMovie_logo.png'
+import TMDBLogo from '../images/tmdb_logo.svg'
+
 const Header = () => {
     return (
         <div>
-            Header
+            <div className="header-content">
+                <img src={RMDBLogo} alt="rmdb-logo" />
+                <img src={TMDBLogo} alt="tmdb-logo" />
+            </div>
         </div>
     )
 }
