7cb6322854c7b9b7d77a94201bc3bcf1031b2ea4
appdevdude7 <appdeveloper.akshay@gmail.com>
2020-02-09 14:19:25 -0600

    updated header component

--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -3,14 +3,16 @@ import React from 'react'
 import RMDBLogo from '../images/reactMovie_logo.png'
 import TMDBLogo from '../images/tmdb_logo.svg'
 
+import {StyledHeader, StyledRMDBLogo, StyledTMDBLogo} from '../styles/StyledHeader'
+
 const Header = () => {
     return (
-        <div>
+        <StyledHeader>
             <div className="header-content">
-                <img src={RMDBLogo} alt="rmdb-logo" />
-                <img src={TMDBLogo} alt="tmdb-logo" />
+                <StyledRMDBLogo src={RMDBLogo} alt="rmdb-logo" />
+                <StyledTMDBLogo src={TMDBLogo} alt="tmdb-logo" />
             </div>
-        </div>
+        </StyledHeader>
     )
 }
 
