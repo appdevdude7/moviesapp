2ce7529bc79ac85d4056d75f76097e660bc85278
appdevdude7 <appdeveloper.akshay@gmail.com>
2020-02-09 14:29:33 -0600

    updated heroimage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import {
+    IMAGE_BASE_URL,
     API_BASE_URL,
     POSTER_SIZE,
     BACKDROP_SIZE
@@ -19,9 +20,16 @@ const Home = () => {
     
     const [{state, loading, error}, fetchMovies] = useHomeFetch();
 
+    if(error) return <div>Something went wrong ..</div>;
+    if(!state.movies[0]) return <Spinner />;
+
     return (
         <>
-            <HeroImage />
+            <HeroImage 
+                image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.heroImage.backdrop_path}`} 
+                title={state.heroImage.original_title}
+                text={state.heroImage.overview}
+            />
             <SearchBar />
             <Grid />
             <MovieThumb />
