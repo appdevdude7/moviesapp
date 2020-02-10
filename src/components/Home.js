4687803a61b805053cd54edce4839edb590b8eff
appdevdude7 <appdeveloper.akshay@gmail.com>
2020-02-10 07:55:42 -0600

    created props for grid and moviethumb

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import {
     IMAGE_BASE_URL,
@@ -16,9 +16,12 @@ import LoadMoreBtn from './elements/LoadMoreBtn'
 
 import { useHomeFetch } from './hooks/useHomeFetch'
 
+import NoImage from './images/no_image.jpg'
+
 const Home = () => {
     
     const [{state, loading, error}, fetchMovies] = useHomeFetch();
+    const [searchTerm, setSearchTerm] = useState('');
 
     if(error) return <div>Something went wrong ..</div>;
     if(!state.movies[0]) return <Spinner />;
@@ -31,8 +34,20 @@ const Home = () => {
                 text={state.heroImage.overview}
             />
             <SearchBar />
-            <Grid />
-            <MovieThumb />
+            <Grid header={ searchTerm ? 'Search Result' : 'Popular Movies' }>
+                {state.movies.map(movie => (
+                    <MovieThumb 
+                        key={movie.id}
+                        clickable
+                        image={ movie.poster_path 
+                            ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
+                            : NoImage
+                         } 
+                        movieId = {movie.id}
+                        movieName={movie.original_title}
+                    />
+                )) }
+            </Grid>
             <Spinner />
             <LoadMoreBtn />
         </>
