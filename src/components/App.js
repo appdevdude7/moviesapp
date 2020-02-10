48d12f97f80adea232e887da917bdfda8b74aef1
appdevdude7 <appdeveloper.akshay@gmail.com>
2020-02-10 07:41:36 -0600

    added globalstyle and spinner styling

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,15 @@ import React from 'react';
 import Header from './elements/Header';
 import Home from './Home'
 
+import { GlobalStyle } from './styles/GlobalStyle'
+
 function App() {
   return (
-    <div>
+    <>
       <Header />
       <Home />
-    </div>
+      <GlobalStyle />
+    </>
   );
 }
 
--- src/components/elements/Spinner.js
+++ src/components/elements/Spinner.js
@@ -1,10 +1,9 @@
 import React from 'react'
+import { StyledSpinner } from '../styles/StyledSpinner'
 
 const Spinner = () => {
     return (
-        <div>
-            Spinner
-        </div>
+        <StyledSpinner />
     )
 }
 
